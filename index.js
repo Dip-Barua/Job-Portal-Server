38d4074600ded4434372f852ca780e40c30b1ab5
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-13 12:34:56 +0600

    Added : Get operation agaist specific job id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -44,6 +44,13 @@ app.get('/jobs', async (req, res) => {
     res.send(result);
 })
 
+app.get('/jobs/:id' , async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await jobsCollection.findOne(query);
+  res.send(result);
+  }
+)
 
 
   } finally {
